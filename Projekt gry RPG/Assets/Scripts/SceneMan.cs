77662cb2ed0103e68da1f5039e1bf61a5294ee56
agatoneq <agata.sobczyk444@wp.pl>
2024-04-24 23:21:30 +0200

    ekran ładowania

--- Projekt gry RPG/Assets/Scripts/SceneMan.cs
+++ Projekt gry RPG/Assets/Scripts/SceneMan.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneMan : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider loadingSlider;
+
     public void ClickPlayButton()
     {
-        SceneManager.LoadScene(2);
+        StartCoroutine(LoadAsynchronously(2));
+
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        UnityEngine.AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            loadingSlider.value = progress;
+
+            yield return null;
+        }
     }
     
     public void ClickContinueButton()
