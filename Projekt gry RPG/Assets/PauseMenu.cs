5271fa438811c5ba1edf6a7da59cf21a74ee404e
agatoneq <agata.sobczyk444@wp.pl>
2024-04-28 02:13:44 +0200

    UI i Menu Pauzy

--- Projekt gry RPG/Assets/PauseMenu.cs
+++ Projekt gry RPG/Assets/PauseMenu.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public static bool IsGamePaused = false;
+    public static bool IsMouseActive = false;
     public GameObject PauseMenuUI;
+    public GameObject SettingsMenuUI;
+    public GameObject MainPanel;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.M))
+            if (IsMouseActive)
+            {
+                DeactivateMouse();
+            }
+            else
+            {
+                ActivateMouse();
+            }
+
         if (Input.GetKeyDown(KeyCode.Escape))
             if (IsGamePaused)
             {
@@ -21,8 +34,24 @@ public class PauseMenu : MonoBehaviour
             }
     }
 
+    void ActivateMouse()
+    {
+        IsMouseActive = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void DeactivateMouse()
+    {
+        IsMouseActive = false;
+        Cursor.visible = false;
+    }
+
+
     public void Resume()
     {
+        SettingsMenuUI.SetActive(false);
+        MainPanel.SetActive(true);
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         IsGamePaused = false;
@@ -32,11 +61,18 @@ public class PauseMenu : MonoBehaviour
     void Pause()
     {
         PauseMenuUI.SetActive(true);
+        SettingsMenuUI.SetActive(false);
         Time.timeScale = 0f;
         IsGamePaused = true;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
+    }
+
+    public void ShowSettings()
+    {
+        SettingsMenuUI.SetActive(true);
+        MainPanel.SetActive(false);
     }
 
 }
