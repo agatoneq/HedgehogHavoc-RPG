0b26aacdb2a9d00770842c616e678ffc91b160f2
Mily <miwojtanek@interia.pl>
2024-05-15 22:51:20 +0200

    UI i animacje w jaskini plus alfa drugiego dialogu

---
+++ Projekt gry RPG/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueeTrigger : MonoBehaviour
+{
+[Header("Ink JSON")]
+[SerializeField] private TextAsset inkJSON;
+}
