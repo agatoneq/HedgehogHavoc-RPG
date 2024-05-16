ee50090fc394d0672c73393542daeef4bae80e47
agatoneq <agata.sobczyk444@wp.pl>
2024-05-17 00:09:10 +0200

    poprawiony ekran smierci :))) i health bar w cavie (mozna umierac)

--- Projekt gry RPG/Assets/Scripts/DeathScreen.cs
+++ Projekt gry RPG/Assets/Scripts/DeathScreen.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
 
     [SerializeField] private GameObject DeathPanel;
+
+    public GameObject loadingPanel;
+    public Slider loadingSlider;
 
     private bool PanelIsActive;
     // Start is called before the first frame update
@@ -34,16 +38,17 @@ public class DeathScreen : MonoBehaviour
     }
 
 
-
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
         UnityEngine.AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-
+        loadingPanel.SetActive(true);
 
         while (!operation.isDone)
         {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
 
+            loadingSlider.value = progress;
 
             yield return null;
         }
