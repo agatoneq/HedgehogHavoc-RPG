d0c857df4279deefe7d19307bfec50a4ee4d1f80
KamilaMlyn <github@kamilamlynarczyk.pl>
2024-06-05 16:23:13 +0200

    ubrania głównego bohatera

--- Projekt gry RPG/Assets/Scripts/HedgehogAppearance.cs
+++ Projekt gry RPG/Assets/Scripts/HedgehogAppearance.cs
@@ -7,21 +7,47 @@ using UnityEngine.UI;
 public class HedgehogClothes : MonoBehaviour
 {
     public GameObject[] Items;
+    public GameObject Armor_helmet;
+    public GameObject Armor_breastplate;
     private int currentItem = 0;
     public int nrTab;
 
     // Start is called before the first frame update
     void Start()
-    { 
-        foreach(var item in Items)
+    {
+        if (Items.Length != 0) //jesli Jezy jest w kreatorze
         {
-            item.SetActive(false);
+            foreach (var item in Items)
+            {
+                item.SetActive(false);
+            }
+            if (currentItem < Items.Length)
+            {
+                Items[currentItem].SetActive(true);
+                PlayerManager.characterLook[nrTab] = Items[currentItem].name;
+                Debug.Log("Apperence: Start() -> kreator: set active: " + Items[currentItem].name + "\n");
+                
+            }
         }
-        if (currentItem < Items.Length)
+        else if(this.gameObject.scene.name != "CharacterCreator") //jesli Jezy jest na scenie
         {
-            Items[currentItem].SetActive(true);
-            PlayerManager.characterLook[nrTab] = Items[currentItem];
+            Debug.Log("Apperance: Start() -> na scenie, ilosc w liscie: "+ PlayerManager.characterLook.Length);
+            foreach (var c in PlayerManager.characterLook)
+            {
+                Transform obj = transform.Find(c);
+                if (obj != null)
+                {
+                    GameObject gameObj = obj.gameObject;
+                    gameObj.SetActive(true);
+                }
+            }
+            if (PlayerManager.armor && Armor_breastplate != null & Armor_helmet != null)
+            {
+                Armor_helmet.SetActive(true);
+                Armor_breastplate.SetActive(true);
+            }
         }
+
     }
     public void Change(bool next) //next: true -> przycisk w przod, false -> w tyl
     {
@@ -40,7 +66,7 @@ public class HedgehogClothes : MonoBehaviour
             else
                 currentItem = Items.Length - 1;
         }
-        PlayerManager.characterLook[nrTab] = Items[currentItem];  //tablica z wybranymi elementami
+        PlayerManager.characterLook[nrTab] = Items[currentItem].name;  //tablica z wybranymi elementami
         
         Items[currentItem].SetActive(true); //pokazanie wybranego itemku
     }
