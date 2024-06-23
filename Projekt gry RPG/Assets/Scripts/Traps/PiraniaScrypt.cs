c2d7bc4eea198d4aa038e29ad0b065066b9d6ab9
Mily <miwojtanek@interia.pl>
2024-06-24 00:19:29 +0200

    Pułapka 3 (piranie)
    Spokojnie są wege

---
+++ Projekt gry RPG/Assets/Scripts/Traps/PiraniaScrypt.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiraniaScrypt : MonoBehaviour
+{
+    // Promień półkola
+    public float radius = 5f;
+    // Prędkość ruchu
+    public float speed = 1f;
+    // Punkt środkowy półkola
+    private Vector3 center;
+
+    void Start()
+    {
+        // Ustaw środek półkola na aktualną pozycję obiektu
+        center = transform.position;
+    }
+
+    void Update()
+    {
+        // Oblicz nową pozycję na półkolu
+        float angle = Mathf.PI * Mathf.Sin(Time.time * speed);
+        float x = center.x + radius * Mathf.Cos(angle);
+        float y = center.y + radius * Mathf.Sin(angle);
+        float z = center.z;
+
+        // Ustaw nową pozycję obiektu
+        transform.position = new Vector3(x, y, z);
+    }
+}
